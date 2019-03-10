53b5a33880253d79a3a44a2b5d7bbf9a370155e4
Hilmar Orth <hm.orth@aol.com>
2019-03-10 07:21:09 +0100

    Submission

--- app.js
+++ app.js
@@ -1,74 +1,138 @@
-// Declarations
 // Maker
 const Maker = require('@makerdao/dai');
+const readline = require('readline');
+// const ethLend = require('./stuff');
+
+const currentId = 5293
+
 require('dotenv').config()
+const {
+  MKR,
+  DAI,
+  ETH,
+  WETH,
+  PETH,
+  USD_ETH,
+  USD_MKR,
+  USD_DAI,
+  buildTestService
+} = Maker;
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-console.log("######")
-//console.log(process.env.PRIVATE_KEY)
-//console.log(`https://kovan.infura.io/v3/${process.env.INFURA_ID}`)
+// Varbiable Declarations
+let cdp;
+let cdpId;
 
+// ############ SET UP ############
 
-// 1. Ask for users CDP ID with button called "check refinancing opportunities"
+async function setUp() {
 
-// 2. Fetch & Display collateral, colla ratio & dai debt of current CDP
+  const maker = await Maker.create("http", {
+    privateKey: process.env.PRIVATE_KEY,
+    url: `https://kovan.infura.io/v3/${process.env.INFURA_ID}`,
+    provider: {
+      type: 'HTTP', // or 'TEST'
+      network: 'kovan'
+    },
+  });
 
-// 3. Fetch current Maker stability fee and display it
-
-// 4. Fetch current ETHLend lending offers (rate + Maximum amount)
-
-// 5. Display EthLend offers and ask if user wants to refinance loan
-
-// 6. If yes, show the accrued cost of the Maker CDP => Dai debt plus accrued interest fee
-
-// Ask for CDP ID
-
-// If CDP exist, fetch Maker stability fee & 
-
-async function openLockDraw() {
-    const maker = await Maker.create("http", {
-        privateKey: process.env.PRIVATE_KEY,
-        url: `https://kovan.infura.io/v3/${process.env.INFURA_ID}`
-    });
-
-  // makes sure all services are initialized
   await maker.authenticate();
 
   // Create a new CDP Object
-  // // const cdp = await maker.openCdp();
-  
-  // 1. Ask for users CDP ID with button called "check refinancing opportunities"
+  cdp = await maker.openCdp();
+  console.log(`CDP successfully created. ID: ${cdp.id}`)
 
-  const cdp = await maker.getCdp(5284);
-  console.log(cdp.id);
+  console.log(`-----------------`);
+
+  console.log(`Putting 0.3 ETH in CDP`);
+  await cdp.lockEth(0.3, ETH);
+  console.log(`done locking eth`);
+
+  console.log(`-----------------`);
+
+  console.log(`Drawing 6 dai`);
+  await cdp.drawDai('6')
+  .then(result => {
+    console.log(result.toString())
+    console.log(`done drawing dai`);
+  })
+
+  console.log(`-----------------`);
+  console.log(`### SET UP END ###`)
+      
+}
+
+
+// ##################### Get CDP id ##############
+
+// 1. Ask for users CDP ID
+function getCdpId() {
+  // Prompt User for CDP number:
+  rl.question('What is your CDP id? ', (answer) => {
+    // TODO: Log the answer in a database
+    console.log(`Your CDP id is: ${answer}`);
+    cdpId = answer
+    rl.close();
+  });
+  return answer
+}
+
+
+async function showMakerInfo(id) {
+  
+  const maker = await Maker.create("http", {
+    privateKey: process.env.PRIVATE_KEY,
+    url: `https://kovan.infura.io/v3/${process.env.INFURA_ID}`,
+    provider: {
+      type: 'HTTP', // or 'TEST'
+      network: 'kovan'
+    },
+  });
+  // makes sure all services are initialized
+  await maker.authenticate();
+
+  // 2. Find CDP and display details
+
+  cdp = await maker.getCdp(parseInt(id));
 
   // 2. Fetch & Display collateral, colla ratio & dai debt of current CDP
 
   let cdpColla;
   const ethCollateral = await cdp.getCollateralValue()
   .then(response => {
-    cdpColla = response['_amount']['c'][0];
+    cdpColla = response.toString()
     
   });
-  console.log(cdpColla);
+  console.log(`CDP Collateral Wei: ${cdpColla}`);
+
+  console.log(`-----------------`);
+
   let cdpCollaEth = cdpColla / 10**14
-  console.log(cdpCollaEth);
+  console.log(`CDP Collateral: ${cdpCollaEth}`);
 
   // Colla Ratio
   let collaRatio;
   const ratio = await cdp.getCollateralizationRatio()
   .then(response => {
     collaRatio = response;
-    console.log(collaRatio)
+    console.log(`Collateralization Ratio: ${collaRatio}`);
   });
+
+  console.log(`-----------------`);
 
   // Dai Debt
   let daiDebt;
   const debt = await cdp.getDebtValue()
   .then(response => {
-    daiDebt = response['_amount']['c'][0] + response['_amount']['c'][1] / 10**14;
-    console.log(daiDebt)
+    daiDebt = response.toString()
+    console.log(`Dai Debt: ${daiDebt}`);
   });
+
+  console.log(`-----------------`);
 
   // Accrued stability fees
   // Cant test that right now
@@ -77,8 +141,11 @@ async function openLockDraw() {
   .then(result => {
     //console.log("Accrued Fees:")
     //console.log(result)
-    accruedFees = result;
+    accruedFees = result.toString();
   })
+  console.log(`Governance Fees (USD): ${accruedFees}`);
+
+  console.log(`-----------------`);
 
   // 3. Fetch current Maker stability fee and display it
 
@@ -90,23 +157,51 @@ async function openLockDraw() {
   let stabilityFee;
   const fee = await ethCdp.getAnnualGovernanceFee()
   .then(result => {
-    console.log(result);
     stabilityFee = result
+    console.log(`Maker Stability Fee: ${stabilityFee}`);
   })
 
+  console.log(`-----------------`);
 }
-// openLockDraw();
 
-// ###############################
-// Aaeve
+// Assuming that the user agreed to refinance their loan to ethlend, require the user to wipe all debt in their cdp
+async function repayCdp() {
+  cdp.shut()
+  console.log("CDP closed")
+}
 
+// setUp()
 
-//console.log(aaeve);
-//const Marketplace = aaeve.Marketplace
-//console.log(Marketplace)
+// ##################### Get CDP id ##############
 
-// 1. Log User In
+// 1. Ask for users CDP ID
+function refinance() {
+  string = "######### Would you like to refinance your variable CDP for a fixed Interest Rate loan? (y/n) ####### "
+  // Prompt User for CDP number:
+  rl.question(string, (answer) => {
+    // TODO: Log the answer in a database
+    console.log(`Great, fetching the ETHLends ETH / DAI Loans...`);
+    rl.close();
+  });
+  return answer
+}
 
-// 2. Fetch current ETHLend lending offers (rate + Maximum amount)
+function startProgram() {
+  console.log("########################################################")
+  console.log("-----------------WELCOME TO LEHMAN BROS-----------------")
+  console.log("     YOUR GO TO SERVICE FOR CRYPTO INTEREST RATE SWAPS")
+  console.log("########################################################")
 
-// 3. Display EthLend offers (if available) and ask if user wants to refinance loan 
+  console.log("")
+  console.log("-----------------Check your MakerCDP Status-----------------")
+
+  id = getCdpId();
+
+  showMakerInfo(id)
+
+  refinance();
+
+  //ethLend.getLoans
+
+}
+
